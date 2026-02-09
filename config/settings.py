d14e1ab11d8885df77e22abeeb70ab67a6c04f57
YONTZ <yontz@example.com>
2026-02-09 22:32:32 +0900

    change a little

--- config/settings.py
+++ config/settings.py
@@ -42,7 +42,7 @@ else:
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY', default="dummy-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = env('DEBUG')
 
