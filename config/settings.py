6bd8b9e4a1a394447ece179495b6b284ff4594f8
YONTZ <yontz@example.com>
2026-02-09 22:10:00 +0900

    for App Runner deployment

--- config/settings.py
+++ config/settings.py
@@ -21,7 +21,7 @@ from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DEBUG = True
+DEBUG = False
 
 # 環境変数の読み込み (ローカル環境のみ)
 if DEBUG:
