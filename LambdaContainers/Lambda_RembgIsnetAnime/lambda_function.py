425510f66ba7da0ef8b60c3f5a94f9c4b871412e
YONTZ <yontz@example.com>
2026-03-05 23:48:13 +0900

    examine workflow

--- LambdaContainers/Lambda_RembgIsnetAnime/lambda_function.py
+++ LambdaContainers/Lambda_RembgIsnetAnime/lambda_function.py
@@ -1,22 +1,18 @@
-import json
 import os
-import io
-import boto3
-import base64
-import requests
-import uuid
-import glob
-
-
-s3 = boto3.client("s3")
-
-
 # --- Numbaのエラーと遅延を解消する設定 ---
 os.environ["NUMBA_CACHE_DIR"] = "/tmp/numba_cache"
 os.environ["NUMBA_THREADING_LAYER"] = "workqueue" # ロックエラーを防ぐ
 os.environ["OMP_NUM_THREADS"] = "1"
 os.environ["MKL_NUM_THREADS"] = "1"
 os.environ["NUMBA_NUM_THREADS"] = "1"
+import json
+import boto3
+import base64
+import requests
+import uuid
+
+
+s3 = boto3.client("s3")
 
 
 # --- 必須設定 ---
