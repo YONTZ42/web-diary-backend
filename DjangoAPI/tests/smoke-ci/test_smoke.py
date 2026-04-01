eadff8ace5d02186e273baeaee81af290b2b4ac0
YONTZ <yontz@example.com>
2026-04-01 23:46:50 +0900

    change for test

--- DjangoAPI/tests/smoke-ci/test_smoke.py
+++ DjangoAPI/tests/smoke-ci/test_smoke.py
@@ -6,7 +6,7 @@ pytestmark = pytest.mark.django_db
 
 
 def test_health_endpoint_returns_200(api_client):
-    response = api_client.get("/health")
+    response = api_client.get("/healthz")
     assert response.status_code == 200
 
 
