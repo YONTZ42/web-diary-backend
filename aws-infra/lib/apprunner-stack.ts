0e0d52b93f5281a5283ded7fe4fd616a14b04048
YONTZ <yontz@example.com>
2026-04-07 15:43:50 +0900

    workflow

--- aws-infra/lib/apprunner-stack.ts
+++ aws-infra/lib/apprunner-stack.ts
@@ -84,7 +84,7 @@ export class AppRunnerStack extends Stack {
         instanceRoleArn: instanceRole.roleArn,
       },
       healthCheckConfiguration: {
-        protocol: "TCP",
+        protocol: "HTTP",
         path: "/healthz",
         interval: 10,
         timeout: 5,
